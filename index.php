32cbe38c36aa78d54848543ebd072b654886e752
lilllllbrown <lilybrown@tests-MacBook-Pro-2.local>
2015-04-16 19:08:14 -0400

    ]

---
+++ index.php
@@ -0,0 +1,81 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Web Usability Testing</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Lily Brown, Gabriel Aboy & Lynden Prior">
+        <meta name="description" content="Web Usability Testing">
+<link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet"
+              href="style.css"
+              media="screen">
+
+    </head>
+    <body id="home">
+        <article id="main">
+            <h1>Web Usability Test |  <a href="http://www.chinaexpressdelivery.com/shop/">China Express 2</a></h1>
+
+            <nav>
+                <ul id='shadowmenu'>
+                    <li><a href='index.php'>CHANGES</a></li>
+                    <li><a href='videos.php'>VIDEOS</a></li>
+                </ul>
+            </nav>
+
+
+
+
+
+            <h2>Before...</h2>
+            <div class='container1' > 
+                <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/home.jpg" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/home.jpg" width="300" height="200"  alt="Main Page" /></a>
+                <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/contact.jpg" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/contact.jpg" width="300" height="200"  alt="Contact Page" /></a>
+                <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/beds.jpg" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/beds.jpg" width="300" height="200"  alt="Beds Page" /></a>
+                <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/products.jpg" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/products.jpg" width="300" height="200"  alt="Products Page" /></a>
+
+            </div>
+
+
+            <aside id="other">
+                <div class="container">
+                    <h2>5 Changes</h2>
+                    <ol class="ordered-list">
+                        <li>Change 1</li>
+                        <li>Change 2</li>
+                        <li>Change 3</li>
+                        <li>Change 4</li>
+                        <li>Change 5</li>
+
+
+                    </ol>
+                </div>
+            </aside>  
+
+
+            <h2>After...</h2>
+            <div class='container2'>
+                <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/homepage.png" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/homepage.png" width="300" height="200"  alt="Home Page" /></a>
+                <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/homepage_bottom.png" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/homepage_bottom.png" width="300" height="200"  alt="Home Page" /></a>
+                <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/productmenu.png" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/productmenu.png" width="300" height="200"  alt="New Nav" /></a>
+                <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/gallery.png" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/gallery.png" width="300" height="200"  alt="Gallery Page" /></a>
+                <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/product.png" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/product.png" width="300" height="200"  alt="Products Page" /></a>
+
+            </div>
+
+
+        </article>
+
+        
+
+
+
+
+
+        <footer>
+            <p>Lily Brown, Gabriel Aboy & Lynden Prior </p>
+        </footer>
+
+
+    </body>
+</html>
---
+++ videos.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Web Usability Testing</title>
+<meta charset="utf-8">
+<meta name="author" content="Lily Brown, Gabriel Aboy & Lynden Prior">
+<meta name="description" content="Web Usability Testing">
+    
+<link rel="stylesheet"
+      href="style.css"
+      media="screen">
+    
+</head>
+<body id="videos">
+<article id="main">
+    <h1>Web Usability Test |  <a href="http://www.chinaexpressdelivery.com/shop/">China Express 2</a></h1>
+    
+  <nav>
+      <ul id='shadowmenu'>
+          <li><a href='index.php'>CHANGES</a></li>
+          <li><a href='videos.php'>VIDEOS</a></li>
+ </ul>
+        </nav>
+
+    <h2>Videos</h2>
+        <div class="script">
+            <a href='Script.pdf'>Script</a></div>
+        
+        <div class="form">
+            <a href='form.pdf'>Permission Form</a></div>
+        
+        <div class="videos"><ol>
+                <li> <iframe width="560" height="315" src="//www.youtube.com/embed/Ed8kpl6hwYc" frameborder="0" allowfullscreen></iframe></li>
+            
+                <li><iframe width="560" height="315" src="//www.youtube.com/embed/Sctd3y70-60" frameborder="0" allowfullscreen></iframe></li>
+            
+                <li><iframe width="560" height="315" src="//www.youtube.com/embed/ot6QG6vOEHY" frameborder="0" allowfullscreen></iframe></li>
+            </ol>
+
+</div>
+
+        
+        
+<footer>
+    <p>Lily Brown, Gabriel Aboy & Lynden Prior</p>
+</footer>
+
+
+    </body>
